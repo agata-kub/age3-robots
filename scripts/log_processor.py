a68c986dc3ceaf3634a09e4032367925495caff4
Agata Kubiczek <akubicze@student.agh.edu.pl>
2017-12-05 17:47:02 +0100

    Basic log processor

--- scripts/log_processor.py
+++ scripts/log_processor.py
@@ -1,59 +1,24 @@
 import os
+import sys
 
-rootdir ='five_rooms_two_robots_2'
-raw = 'raw'
-processed = 'processed'
-
-# extract all data with [S] tag, and put it into separate .csv files
-def extract_S_data():
-    for subdir, dirs, files in os.walk(rootdir+'/'+raw):
-        for file in files:
-            infile = open(rootdir+'/'+raw+'/'+file,'r')
-            outfile = open(rootdir+'/'+processed+'/'+file,'w')
-            lines = infile.readlines()
-            for line in lines:
-                if line.startswith("[S]"):
-                    data = line.split(';')
-                    resultline = data[1]+','+data[2]+'\n'
-                    outfile.write(resultline)
-            infile.close()
-            outfile.close()
-
-# extract best solution's fitness from all files and put them into one .csv file
-def prepare_table():
-    outfile = open(rootdir+'/'+processed+'/all.csv','w')
-    outfile.write("NUMBER,FITNESS\n")
-    counter = 0
-    for subdir, dirs, files in os.walk(rootdir+'/'+raw):
-        for file in files:
-            infile = open(rootdir+'/'+raw+'/'+file,'r')
-            lines = infile.readlines()
-            lines.reverse()
-            for line in lines:
-                if line.startswith("[S]"):
-                    data = line.split(';')
-                    resultline = str(counter)+','+data[2]+'\n'
-                    outfile.write(resultline)
-                    infile.close()
-                    break
-            counter += 1
+# extract all data with X tag, and put it into outfile
+def extract_tag_data(inpath, outpath, tag):
+    infile = open(inpath,'r')
+    outfile = open(outpath,'w')
+    lines = infile.readlines()
+    for line in lines:
+        if line.startswith("["+tag):
+            data = line.split(';')
+            resultline = ",".join(data[1:])
+            outfile.write(resultline)
+    infile.close()
     outfile.close()
+
+def main():
+    inpath = sys.argv[1]
+    outpath = sys.argv[2]
+    tag = sys.argv[3]
+    extract_tag_data(inpath, outpath, tag)
     
-def generate_for_boxplot():
-    outfile = open(rootdir+'/'+processed+'/boxplot.csv','w')
-    outfile.write("TIME,FITNESS\n")
-    for subdir, dirs, files in os.walk(rootdir+'/'+raw):
-        for file in files:
-            counter = 0
-            infile = open(rootdir+'/'+raw+'/'+file,'r')
-            lines = infile.readlines()
-            for line in lines:
-                if line.startswith("[S]"):
-                    data = line.split(';')
-                    resultline = str(counter)+','+data[2]+'\n'
-                    outfile.write(resultline)
-                    counter += 1     
-            infile.close()
-    outfile.close()
-    
-generate_for_boxplot()
+if __name__ == "__main__":
+    main()
