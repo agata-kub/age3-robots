bad7406044a6d9c73221ac27dbe08e3cc08ea71a
Agata Kubiczek <akubicze@student.agh.edu.pl>
2018-06-18 15:04:01 +0200

    Log processing scripts improvements

--- scripts/adv_logs_processor.py
+++ scripts/adv_logs_processor.py
@@ -1,8 +1,11 @@
 import os
+from collections import OrderedDict
 
-rootdir ='C:/Agata/agh/mgr/age/age3-ogr-labs/age3-robots/archive/2018-05-21/5-50-2'
+rootdir ='C:/Agata/agh/mgr/age/age3-ogr-labs/age3-robots/archive/2018-06-14/sample_1-2-25-2'
+# rootdir ='C:/Agata/agh/mgr/test'
 raw = 'raw'
 processed = 'processed'
+interpolation = 'interpolation'
 
 # extract all data with [S] tag, and put it into separate .csv files
 def extract_S_data():
@@ -36,6 +39,47 @@ def extract_S_data_and_snap_fitness():
             infile.close()
     outfile.close()
 
+# extract all data with [S] tag from all files in raw directory, then get values afor fixed xs as linear interpolation and put it into separate files
+def extract_S_data_and_interpolate():
+    if not os.path.exists(rootdir+'/'+interpolation):
+        os.makedirs(rootdir+'/'+interpolation)
+    for subdir, dirs, files in os.walk(rootdir+'/'+raw):
+        for file in files:
+            step = 100
+            discrete_fitness = 700
+            left = None
+            outfile = open(rootdir+'/'+interpolation+'/interpolation_'+file,'w')
+            outfile.write("EVALUATION_COUNTER,CURRENT_BEST_FITNESS\n")
+            infile = open(rootdir+'/'+raw+'/'+file,'r')
+            lines = infile.readlines()
+            for line in lines:
+                if line.startswith("[S]"):
+                    data = line.split(';')
+                    curr_fitness_count = int(data[3])
+                    curr_fitness_value = data[2]
+                    print("Curr count "+str(curr_fitness_count)+", discrete "+str(discrete_fitness))
+                    if curr_fitness_count==discrete_fitness:
+                        resultline = str(curr_fitness_count)+','+curr_fitness_value+'\n'
+                        outfile.write(resultline)
+                        discrete_fitness += step
+                    elif curr_fitness_count>discrete_fitness:
+                        while curr_fitness_count>discrete_fitness:
+                            x0=left[0]
+                            y0=left[1]
+                            x1=curr_fitness_count
+                            y1=float(curr_fitness_value)
+                            if x0==x1:
+                                print(line)
+                                print(curr_fitness_count)
+                                print(discrete_fitness)
+                            interpolated_fitness_value = y0+(y0-y1)*(discrete_fitness-x0)/(x0-x1)
+                            resultline = str(discrete_fitness)+','+str(interpolated_fitness_value)+'\n'
+                            outfile.write(resultline)
+                            discrete_fitness += step
+                    left = (curr_fitness_count,float(curr_fitness_value))
+            infile.close()
+            outfile.close()
+
 # accepts the file created by extract_S_data_and_snap_fitness(), and transformes duplicate entries into average value
 def map_to_avg():
     infile = open(rootdir+'/snapped.csv','r')
@@ -54,6 +98,33 @@ def map_to_avg():
         outfile.write(snapped_fitness+','+str(current_best_avg)+'\n')
     outfile.close()
     infile.close()
+
+# accepts the files created by extract_S_data_and_interpolate(), and transformes values into min, max and average for each x
+def map_to_min_max_avg():
+    outdata_raw = {}
+    for subdir, dirs, files in os.walk(rootdir+'/'+interpolation):
+        for file in files:
+            infile = open(rootdir+'/'+interpolation+'/'+file,'r')
+            lines = infile.readlines()
+            for line in lines:
+                if not line.startswith("E"):
+                    data = line.split(',')
+                    if not data[0] in outdata_raw:
+                        outdata_raw[data[0]] = []
+                    outdata_raw[data[0]].append(float(data[1]))                        
+            infile.close()
+    outdata_processed = {}
+    for key in outdata_raw:
+        values = outdata_raw[key]
+        outdata_processed[key] = (min(values), sum(values)/len(values), max(values))
+    outfile = open(rootdir+'/interpolated_minmaxavg.csv','w')
+    outfile.write("EVALUATION_COUNTER,CURRENT_MIN,CURRENT_AVG,CURRENT_MAX\n")
+    outdata_sorted = OrderedDict(sorted(outdata_processed.items(), key=lambda t: int(t[0])))
+    for key in outdata_sorted:
+        values = outdata_sorted[key]
+        resultline = key+','+str(values[0])+','+str(values[1])+','+str(values[2])+"\n"
+        outfile.write(resultline)
+    outfile.close()
 
 
 # extract best solution's fitness from all files and put them into one .csv file
@@ -94,5 +165,7 @@ def generate_for_boxplot():
     outfile.close()
     
 # extract_S_data()
-extract_S_data_and_snap_fitness()
-map_to_avg()
+# extract_S_data_and_snap_fitness()
+# map_to_avg()
+extract_S_data_and_interpolate()
+map_to_min_max_avg()
--- scripts/plotly_test.py
+++ scripts/plotly_test.py
@@ -2,29 +2,57 @@ import plotly.offline as offline
 import plotly.plotly as py
 import plotly.graph_objs as go
 
-inpath ='C:/Agata/agh/mgr/age/age3-ogr-labs/age3-robots/archive/2018-05-21/5-50-2/snapped_avg.csv'
+inpath ='C:/Agata/agh/mgr/age/age3-ogr-labs/age3-robots/archive/2018-06-14/sample_1-2-25-2/interpolated_minmaxavg.csv'
 filename='sample_1'
-rtc=5
+rtc=2
 pop=50
 rbc=2
 
-infile = open(inpath,'r')
-lines = infile.readlines()
-eval_count = []
-fitness = []
-for line in lines:
-    if not line.startswith('E'):
-        data = line.split(',')
-        eval_count.append(int(data[0]))
-        fitness.append(float(data[1]))
-infile.close()
+def get_avg_trace():
+    infile = open(inpath,'r')
+    lines = infile.readlines()
+    eval_count = []
+    fitness = []
+    for line in lines:
+        if not line.startswith('E'):
+            data = line.split(',')
+            eval_count.append(int(data[0]))
+            fitness.append(float(data[1]))
+    infile.close()
+    trace = go.Scatter(
+        x=eval_count,
+        y=fitness,
+        mode = 'markers'
+    )
+    return [trace]
 
-trace = go.Scatter(
-    x=eval_count,
-    y=fitness,
-    mode = 'markers'
-)
+def get_minmaxavg_traces():
+    infile = open(inpath,'r')
+    lines = infile.readlines()
+    eval_count = []
+    minf = []
+    avgf = []
+    maxf = []
+    # bfs = []
+    for line in lines:
+        if not line.startswith('E'):
+            data = line.split(',')
+            eval_count.append(int(data[0]))
+            minf.append(float(data[1]))
+            avgf.append(float(data[2]))
+            maxf.append(float(data[3]))
+            # bfs.append(8.7411485E5)
+    infile.close()
+    mintrace = go.Scatter(x=eval_count, y=minf, mode = 'lines+markers', name = 'min')
+    avgtrace = go.Scatter(x=eval_count, y=avgf, mode = 'lines+markers', name = 'avg')
+    maxtrace = go.Scatter(x=eval_count, y=maxf, mode = 'lines+markers', name = 'max')
+    # bfstrace = go.Scatter(x=eval_count, y=bfs, mode = 'lines+markers', name = 'bfs')
+    return [mintrace, avgtrace, maxtrace]
 
+
+
+traces = get_minmaxavg_traces()
+    
 layout = go.Layout(
     title='{} - robots count {} - population {} - routes count {}'.format(filename, rtc, pop, rbc),
     xaxis=dict(
@@ -37,7 +65,7 @@ layout = go.Layout(
 
 graph = offline.plot(
     {
-        "data": [trace],
+        "data": traces,
         "layout": layout
     },
     image='png'
